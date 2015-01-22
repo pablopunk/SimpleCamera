9fd4d36b9a8f0d2efb2f1e689ab55d89fc35a89f
Pablo Varela <pablovarela182@gmail.com>
2015-01-22 19:47:56 +0100

    Initial Commit

---
+++ Simple Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Camera
+//
+//  Created by Pablo Varela on 22/1/15.
+//  Copyright (c) 2015 Pablo Varela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
